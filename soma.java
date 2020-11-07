06909c8877d3a4e76bf52f7aac2af39c54bf7f0c
Roberta Borges <robertav@usp.br>
2020-11-07 19:19:43 -0300

    em andamento

--- soma.java
+++ soma.java
@@ -32,46 +32,9 @@ public class soma {
         return somaB(a, ini, med) + somaB(a, med + 1, fim);
     }
 
-    private class NodeNivel { // classe para ter No e Nivel
-        public Node no;
-        public int nivel;
-
-        public NodeNivel(Node no, int nivel) {
-            this.no = no;
-            this.nivel = nivel;
-        }
-    }
-
-    public int nivel() {
-        Node aux = raiz;
-        int nivel = 0;
-        if (aux == null)
-            throw new IllegalArgumentException("Arvore vazia.");
-        Deque<NodeNivel> fila = new ArrayDeque<>(); // Agora Deque<NodeNivel>
-
-        // cada vez que empilha leva nivel tambem,que é 1 para a raiz
-        fila.add(new NodeNivel(aux, 1));
-
-        while (!fila.isEmpty()) {
-            NodeNivel atual = fila.removeFirst(); // Agora NodeNivel
-
-            // estes ifs agora empilham NodeNivel aumentando o nivel em 1
-            if (atual.no.getNodeEsquerda() != null)
-                fila.add(new NodeNivel(atual.no.getNodeEsquerda(), atual.nivel + 1));
-            if (atual.no.getNodeDireita() != null)
-                fila.add(new NodeNivel(atual.no.getNodeDireita(), atual.nivel + 1));
-
-            // verifica e atualiza o nivel maximo
-            if (atual.nivel > nivel)
-                nivel = atual.nivel;
-        }
-
-        return nivel;
-    }
-
     public static void main(String[] args) {
 
-        int[] a = { 1, 2, 3, 4, 5, 6, 7 };
+        int[] a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
         System.out.println("soma (somaA):\t" + somaA(a, 0, a.length - 1) + " chamadas (somaA):\t" + (contador1));
 
